273e7ee44f3d19ed8e964935825aeef94b08b241
MiguelARam27 <garandster27@live.com>
2020-11-26 22:37:37 -0500

    get user profile info, and update club info added

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -3,9 +3,9 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 import Profile from '../models/ProfileModel.js';
 
-//@ desc POST user profile info
-//@route GET /api/profile
-//access public
+//@desc POST user profile info
+//@route POST /api/profile
+//access private
 const userProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -28,4 +28,57 @@ const userProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
-export { userProfile };
+
+//@desc POST user profile info
+//@route POST /api/profile/clubs
+//access private
+const userProfileClubs = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { clubName, timeAttended } = req.body;
+    const clubInfo = {
+      clubName,
+      timeAttended,
+    };
+
+    let profile = await Profile.findOne({ user });
+    profile.clubs.unshift(clubInfo);
+    await profile.save();
+
+    return res.json(profile);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+//@desc GET user profiles
+//@route GET /api/profile
+//access private
+const userProfiles = asyncHandler(async (req, res) => {
+  const profiles = await Profile.find();
+
+  if (profiles) {
+    res.json(profiles);
+  } else {
+    res.status(404);
+    throw new Error('profiles not found');
+  }
+});
+
+//@desc GET one user profile
+//@route GET /api/profile/:id
+//access private
+const userProfileInfo = asyncHandler(async (req, res) => {
+  const profile = await Profile.findById(req.params.id);
+
+  if (profile) {
+    res.json(profile);
+  } else {
+    res.status(404);
+    throw new Error('profile not found');
+  }
+});
+
+export { userProfile, userProfiles, userProfileInfo, userProfileClubs };
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,7 +1,14 @@
 import express from 'express';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
-import { userProfile } from '../controllers/profileController.js';
+import {
+  userProfile,
+  userProfiles,
+  userProfileInfo,
+  userProfileClubs,
+} from '../controllers/profileController.js';
 
-router.route('/').post(protect, userProfile);
+router.route('/').post(protect, userProfile).get(protect, userProfiles);
+router.route('/clubs').post(protect, userProfileClubs);
+router.route('/:id').get(protect, userProfileInfo);
 export default router;
