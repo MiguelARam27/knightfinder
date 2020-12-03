1508564aa7ef3c2c7d989ee60cc526784dfc136b
MiguelARam27 <garandster27@live.com>
2020-12-02 20:05:33 -0500

    Loading component added, added redux to login

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <>
+      <img src='./img/25.gif' alt='loader' className='Loading' />
+    </>
+  );
+};
+
+export default loading;
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { pageAnimation, fade } from './animation/Animation';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../actions/userActions';
+import Loading from './Loading';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+    dispatch(login(email, password));
+  };
   return (
     <motion.div
       variants={pageAnimation}
@@ -15,19 +32,33 @@ const Login = () => {
         style={{ backgroundImage: "url('./img/student.jpg')" }}
       >
         <div className='login__container'>
-          <form className='form'>
-            <div className='heading'>
-              <h1 className='heading-primary--main'>login</h1>
-            </div>
-            <div className='form__container'>
-              <input type='email' className='form__input' placeholder='email' />
-              <input
-                type='password'
-                className='form__input'
-                placeholder='password'
-              />
-              <button>Login</button>
-            </div>
+          <form className='form' onSubmit={submitHandler}>
+            {loading ? (
+              <Loading />
+            ) : (
+              <>
+                <div className='heading'>
+                  <h1 className='heading-primary--main'>login</h1>
+                </div>
+                <div className='form__container'>
+                  <input
+                    type='email'
+                    className='form__input'
+                    placeholder='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <input
+                    type='password'
+                    className='form__input'
+                    placeholder='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <button>Login</button>
+                </div>
+              </>
+            )}
           </form>
         </div>
       </motion.div>
