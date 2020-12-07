098af89e8ef1b422819eb50dae4ae6e7d830d934
MiguelARam27 <garandster27@live.com>
2020-12-07 00:57:19 -0500

    get user profile info added when logging in.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,9 +4,13 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_RESET,
 } from '../constants/userConstants';
 
-export const login = (email, password) => async (dispatch) => {
+export const login = (email, password) => async (dispatch, getState) => {
   try {
     dispatch({
       type: USER_LOGIN_REQUEST,
@@ -28,6 +32,12 @@ export const login = (email, password) => async (dispatch) => {
       payload: data,
     });
     localStorage.setItem('userInfo', JSON.stringify(data));
+
+    // const {
+    //   userLogin: { userInfo },
+    // } = getState();
+    // console.log(userInfo);
+    dispatch(getUserDetails());
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
@@ -39,7 +49,42 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
+export const getUserDetails = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/users/profile`, config);
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
 };
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -24,16 +24,16 @@ export const userLoginReducer = (state = {}, action) => {
   }
 };
 
-export const userDetailsReducer = (state = { user: {} }, action) => {
+export const userDetailsReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
-      return { ...state, loading: true };
+      return { loading: true };
     case USER_DETAILS_SUCCESS:
-      return { loading: false, user: action.payload };
+      return { loading: false, profileInfo: action.payload };
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     case USER_DETAILS_RESET:
-      return { user: {} };
+      return {};
     default:
       return state;
   }
