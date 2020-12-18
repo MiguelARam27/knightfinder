f5005464c05ffa3b84d02c5ec5ef9024dcb5cc3b
MiguelARam27 <garandster27@live.com>
2020-12-17 21:40:56 -0500

    bugs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
-import User from '../models/userModel.js';
+import User from '../models/UserModel.js';
 import Profile from '../models/ProfileModel.js';
 
 // @desc    Auth user & get token
