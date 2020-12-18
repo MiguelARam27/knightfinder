2905e7a11053633a0e735575ea2a0a6c2d76b884
MiguelARam27 <garandster27@live.com>
2020-12-17 21:33:28 -0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,18 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/upload', uploadRoutes);
-app.get('/', (req, res) => res.send('app is running'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
