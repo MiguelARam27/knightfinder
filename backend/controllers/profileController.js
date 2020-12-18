83c690d60a4fc97bb7ff851aae5ef7dba0592e16
MiguelARam27 <garandster27@live.com>
2020-12-17 21:49:35 -0500

    bug

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler';
 
-import User from '../models/userModel.js';
+import User from '../models/UserModel.js';
 import Profile from '../models/ProfileModel.js';
 
 //@desc POST user profile info
