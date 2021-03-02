c6cc51503d856807b4d0fb9a3a370f299341f8c4
MiguelARam27 <garandster27@live.com>
2021-03-01 23:23:14 -0500

    added sendgrid fixed user controller bug

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,20 +4,39 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: false,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
     },
   });
-
+  console.log(options);
   let message = {
-    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`, // sender address
+    from: `${process.env.FROM_EMAIL}`, // sender address
     to: options.email, // list of receivers
     subject: options.subject, // Subject line
     text: options.message,
   };
-
+  console.log(message);
   const info = await transporter.sendMail(message);
 };
+
+// import sgMail from '@sendgrid/mail';
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'test@example.com', // Change to your recipient
+//   from: 'test@example.com', // Change to your verified sender
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// };
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent');
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
 export default sendEmail;
